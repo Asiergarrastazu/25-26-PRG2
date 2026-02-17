ecdd21bf3eafa92fe76064ad0e4df2c80c5bc3cc
Asier Garrastazu <asier.gc20072gmail.com>
2026-02-17 13:34:36 +0100

    Organizamos el código en metodos estáticos

--- entregas/GarrastazuAsier/Buscaminas.java
+++ entregas/GarrastazuAsier/Buscaminas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Buscaminas{
+public class Buscaminas {
 
 	static final int FILAS = 5;
 	static final int COLUMNAS = 7;
@@ -8,7 +8,7 @@ public class Buscaminas{
 	static final int MAX_MINAS_TOCADAS = 2;
 	static final int MAX_MOVIMIENTOS = 31;
 
-	static String[][] mapaMinasMostrar = 	
+	static String[][] mapaMinasMostrar =
 		{
 			{" ", "1", "2", "3", "4", "5", "6", "7"},
 			{"1", "-", "-", "-", "-", "-", "-", "-"},
@@ -18,7 +18,7 @@ public class Buscaminas{
 			{"5", "-", "-", "-", "-", "-", "-", "-"}
 		};
 		
-	static String[][] mapaMinasActivas = 	
+	static String[][] mapaMinasActivas =
 		{
 			{" ", "1", "2", "3", "4", "5", "6", "7"},
 			{"1", "0", "0", "0", "0", "0", "0", "0"},
@@ -28,66 +28,76 @@ public class Buscaminas{
 			{"5", "0", "0", "0", "0", "0", "0", "0"}
 		};
 
-	public static void main(String[] args){	
-		Scanner scan = new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
 
-		int i = 0;	
-		while (i < MINAS){
-			int PosXMina = (int)(Math.random() * FILAS + 1);
-			int PosYMina = (int)(Math.random() * COLUMNAS + 1);
+	public static void main(String[] args) {
+		colocarMinas();
 
-			if (mapaMinasActivas[PosXMina][PosYMina].equals("1")){
-				continue;
-			} else {
-				mapaMinasActivas[PosXMina][PosYMina] = "1";
-				i++;
-			}
-		}
-
-		int posX = 0, posY = 0;
-		int juegoEncendido = 1;
-		int valoresCorrectos;
-		int contadorMapa = 0;
 		int contadorMinas = 0;
+		int contadorMapa = 0;
+		boolean juegoEncendido = true;
 
-		while(juegoEncendido == 1){
+		while (juegoEncendido) {
+			imprimirTablero();
+			int[] coordenadas = leerCoordenadas();
 
-			for(i = 0; i <= FILAS; i++){            
-				for (int j = 0; j <= COLUMNAS; j++){                                     
-					System.out.print(mapaMinasMostrar[i][j] + " ");
-				}
-				System.out.println();
-			}
-
-			valoresCorrectos = 0;
-
-			while(valoresCorrectos == 0){
-				posY = scan.nextInt();
-				posX = scan.nextInt();
-
-				if(posX >= 1 && posX <= FILAS && posY >= 1 && posY <= COLUMNAS){
-					valoresCorrectos = 1;
-				}
-			}
-
-			if(!mapaMinasMostrar[posX][posY].equals("-")){
+			if (!mapaMinasMostrar[coordenadas[0]][coordenadas[1]].equals("-")) {
 				continue;
 			}
 
-			if(mapaMinasActivas[posX][posY].equals("1")){
-				mapaMinasMostrar[posX][posY] = "x";
+			procesarJugada(coordenadas);
+
+			if (mapaMinasActivas[coordenadas[0]][coordenadas[1]].equals("1")) {
 				contadorMinas++;
-			} else if(mapaMinasActivas[posX][posY].equals("0")){
-				mapaMinasMostrar[posX][posY] = ".";
 			}
 
 			contadorMapa++;
+			juegoEncendido = !(contadorMinas > MAX_MINAS_TOCADAS || contadorMapa >= MAX_MOVIMIENTOS);
+		}
+	}
 
-			if(contadorMinas > MAX_MINAS_TOCADAS){
-				juegoEncendido = 0;
-			} else if(contadorMapa >= MAX_MOVIMIENTOS){
-				juegoEncendido = 0;
+	static void colocarMinas() {
+		int i = 0;
+		while (i < MINAS) {
+			int posX = (int) (Math.random() * FILAS + 1);
+			int posY = (int) (Math.random() * COLUMNAS + 1);
+			if (!mapaMinasActivas[posX][posY].equals("1")) {
+				mapaMinasActivas[posX][posY] = "1";
+				i++;
 			}
 		}
-	}	
+	}
+
+	static void imprimirTablero() {
+		for (int i = 0; i < mapaMinasMostrar.length; i++) {
+			for (int j = 0; j < mapaMinasMostrar[i].length; j++) {
+				System.out.print(mapaMinasMostrar[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	static int[] leerCoordenadas() {
+		int posX = 0, posY = 0;
+		boolean valido = false;
+		while (!valido) {
+			posY = scan.nextInt();
+			posX = scan.nextInt();
+			if (posX >= 1 && posX <= FILAS && posY >= 1 && posY <= COLUMNAS) {
+				valido = true;
+			}
+		}
+		return new int[]{posX, posY};
+	}
+
+	static void procesarJugada(int[] coordenadas) {
+		int posX = coordenadas[0];
+		int posY = coordenadas[1];
+
+		if (mapaMinasActivas[posX][posY].equals("1")) {
+			mapaMinasMostrar[posX][posY] = "x";
+		} else if (mapaMinasActivas[posX][posY].equals("0")) {
+			mapaMinasMostrar[posX][posY] = ".";
+		}
+	}
 }
